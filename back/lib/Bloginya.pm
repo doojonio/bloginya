b6e73bb9efe40236c5cb3d6e5c8d073de6668e3d
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-05-22 17:01:03 +0000

    docs(back)

--- back/lib/Bloginya.pm
+++ back/lib/Bloginya.pm
@@ -9,6 +9,7 @@ sub startup ($self) {
   my $config = $self->plugin('NotYAMLConfig');
   $self->secrets($config->{secrets});
 
+  # Default helpers
   $self->plugin('DefaultHelpers');
 
   # for files 1gb
