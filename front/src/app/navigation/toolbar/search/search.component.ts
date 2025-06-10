451b9281baffe40368e1ca466228e2a226bb4049
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-06-10 21:26:18 +0500

    f

---
+++ front/src/app/navigation/toolbar/search/search.component.ts
@@ -0,0 +1,57 @@
+import { AsyncPipe } from '@angular/common';
+import { Component, input } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+
+export interface User {
+  name: string;
+}
+
+@Component({
+  selector: 'app-search',
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatAutocompleteModule,
+    ReactiveFormsModule,
+    AsyncPipe,
+  ],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.scss',
+})
+export class SearchComponent {
+  version = input<'desktop' | 'handset'>('desktop');
+
+  myControl = new FormControl<string | User>('');
+  options: User[] = [{ name: 'Mary' }, { name: 'Shelley' }, { name: 'Igor' }];
+  filteredOptions!: Observable<User[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => {
+        const name = typeof value === 'string' ? value : value?.name;
+        return name ? this._filter(name as string) : this.options.slice();
+      })
+    );
+  }
+
+  displayFn(user: User): string {
+    return user && user.name ? user.name : '';
+  }
+
+  private _filter(name: string): User[] {
+    const filterValue = name.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.name.toLowerCase().includes(filterValue)
+    );
+  }
+}
--- front/src/app/navigation/toolbar/toolbar.component.ts
+++ front/src/app/navigation/toolbar/toolbar.component.ts
@@ -1,14 +1,21 @@
-import { AsyncPipe, CommonModule } from '@angular/common';
+import { AsyncPipe } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDrawer } from '@angular/material/sidenav';
-import { AppService } from '../../app.service';
 import { RouterModule } from '@angular/router';
+import { AppService } from '../../app.service';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [MatIconModule, AsyncPipe, MatButtonModule, RouterModule],
+  imports: [
+    MatIconModule,
+    AsyncPipe,
+    MatButtonModule,
+    RouterModule,
+    SearchComponent,
+  ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss',
 })
