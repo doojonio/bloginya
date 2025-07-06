52acba7826695a46cd16c4ebc0cc69a2c9c5fffc
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-07-06 17:17:41 +0500

    f

--- front/src/app/view/post-list-category/post-list-category.component.ts
+++ front/src/app/view/post-list-category/post-list-category.component.ts
@@ -1,18 +1,43 @@
 import { UpperCasePipe } from '@angular/common';
-import { Component, input, model } from '@angular/core';
+import { Component, inject, input, model } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { MatChipListboxChange, MatChipsModule } from '@angular/material/chips';
-import { Category, CatPost } from '../../posts.service';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
 import { RouterModule } from '@angular/router';
-import { PostListMedComponent } from "../post-list-med/post-list-med.component";
+import { AppService } from '../../app.service';
+import { Category, CatPost, picStyle } from '../../posts.service';
+import { PostListMedComponent } from '../post-list-med/post-list-med.component';
 
 @Component({
   selector: 'app-post-list-category',
-  imports: [MatChipsModule, UpperCasePipe, RouterModule, PostListMedComponent],
+  imports: [
+    MatChipsModule,
+    UpperCasePipe,
+    RouterModule,
+    PostListMedComponent,
+    MatTabsModule,
+  ],
   templateUrl: './post-list-category.component.html',
   styleUrl: './post-list-category.component.scss',
 })
 export class PostListCategoryComponent {
+  linkForPost(post: CatPost) {
+    return post.name ? '/' + post.name : '/p/' + post.id;
+  }
+  imageUrl(post: CatPost) {
+    return picStyle(post.picture_pre, 'medium');
+  }
+  onChangeTab($event: MatTabChangeEvent) {
+    let cats = this.cats();
+    this.selectedCat.set(cats[$event.index]);
+  }
+  private readonly appService = inject(AppService);
+  isHandset = toSignal(this.appService.isHandset());
+
   onChange(newLang: MatChipListboxChange) {
+    if (!newLang.value) {
+      return;
+    }
     this.selectedCat.update((old) => newLang.value);
   }
 
