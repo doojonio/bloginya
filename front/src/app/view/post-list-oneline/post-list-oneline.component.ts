4e13c8869242f98145a4eb71ec5f1375600398d1
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-07-06 16:37:28 +0500

    bts

--- front/src/app/view/post-list-oneline/post-list-oneline.component.ts
+++ front/src/app/view/post-list-oneline/post-list-oneline.component.ts
@@ -1,25 +1,41 @@
-import { UpperCasePipe } from '@angular/common';
+import { AsyncPipe, UpperCasePipe } from '@angular/common';
 import {
   Component,
+  computed,
   effect,
   ElementRef,
+  inject,
   input,
   signal,
+  ViewChild,
   viewChildren,
 } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
+import { AppService } from '../../app.service';
 import { picStyle } from '../../posts.service';
 
 @Component({
   selector: 'app-post-list-oneline',
-  imports: [UpperCasePipe, RouterModule],
+  imports: [
+    UpperCasePipe,
+    RouterModule,
+    AsyncPipe,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './post-list-oneline.component.html',
   styleUrl: './post-list-oneline.component.scss',
 })
 export class PostListOnelineComponent {
   title = input<string>();
   posts = input.required<PostOneLinePost[]>();
+  postsLength = computed(() => this.posts().length);
   titles = viewChildren<ElementRef>('postTitle'); // Signal<ReadonlyArray<ElementRef>>
+
+  private readonly appService = inject(AppService);
+  isHandset$ = this.appService.isHandset();
 
   minHeight = signal('0px');
 
@@ -38,6 +54,26 @@ export class PostListOnelineComponent {
 
   getPostPreStyle(post: PostOneLinePost) {
     return { background: picStyle(post.picture_pre, 'medium') };
+  }
+
+  /* DEKSTOP
+  */
+  @ViewChild('postsContainer') postsContainer!: ElementRef<HTMLDivElement>;
+
+  scrollPrev(): void {
+    if (this.postsContainer) {
+      const container = this.postsContainer.nativeElement;
+      // Scroll left by the container's visible width for a "page" effect
+      container.scrollBy({ left: -container.clientWidth, behavior: 'smooth' });
+    }
+  }
+
+  scrollNext(): void {
+    if (this.postsContainer) {
+      const container = this.postsContainer.nativeElement;
+      // Scroll right by the container's visible width
+      container.scrollBy({ left: container.clientWidth, behavior: 'smooth' });
+    }
   }
 }
 
