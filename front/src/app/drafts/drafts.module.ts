c0b33533eacf9a14080a14a9b48a16db92dc6817
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-07-13 20:35:57 +0500

    fix: drafts page

--- front/src/app/drafts/drafts.module.ts
+++ front/src/app/drafts/drafts.module.ts
@@ -4,9 +4,11 @@ import { RouterModule } from '@angular/router';
 import { PostListGridTitlesComponent } from '../shared/components/post-list-grid-titles/post-list-grid-titles.component';
 import { routes } from './drafts.routes';
 import { DraftsComponent } from './drafts/drafts.component';
+import { DraftsService } from './services/drafts.service';
 
 @NgModule({
   declarations: [DraftsComponent],
+  providers: [DraftsService],
   imports: [
     CommonModule,
     AsyncPipe,
