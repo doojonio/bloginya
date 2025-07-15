1c2883c9fe6ca6667235581ccecceb33707e40c3
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-07-15 16:26:01 +0000

    feature: lang by char count

--- back/lib/Bloginya/Service/Language.pm
+++ back/lib/Bloginya/Service/Language.pm
@@ -2,6 +2,7 @@ package Bloginya::Service::Language;
 use Mojo::Base -base, -signatures, -async_await;
 
 has 'se_google';
+has 'log';
 
 use constant {CHINESE => 'zh', RUSSIAN => 'ru', ENGLISH => 'en', JAPANESE => 'ja', KOREAN => 'ko'};
 
@@ -23,28 +24,44 @@ async sub detect_by_google_translate_p($self, $txt) {
 sub detect_by_category ($self, $category) {
   return unless $category;
   for (qw(CHINESE RUSSIAN ENGLISH JAPANESE KOREAN)) {
-    if ($category =~ /$_/i) {
+    if ($category =~ /^$_$/i) {
       return __PACKAGE__->can($_)->();
     }
   }
 }
 
 sub detect_by_chars ($self, $txt) {
-  if ($txt =~ /\p{Script=Hiragana}|\p{Script=Katakana}/) {
-    return JAPANESE;
+
+  # detect by count of characters
+  my %count;
+  for my $char (split //, $txt) {
+    if ($char =~ /\p{Script=Hiragana}|\p{Script=Katakana}/) {
+      $count{&JAPANESE()}++;
+    }
+    elsif ($char =~ /\p{Script=Han}/) {
+      $count{&CHINESE()}++;
+    }
+    elsif ($char =~ /\p{Script=Hangul}/) {
+      $count{&KOREAN()}++;
+    }
+    elsif ($char =~ /\p{Script=Cyrillic}/) {
+      $count{&RUSSIAN()}++;
+    }
+    elsif ($char =~ /\p{Script=Latin}/) {
+      $count{&ENGLISH()}++;
+    }
   }
-  elsif ($txt =~ /\p{Script=Han}/) {
-    return CHINESE;
+
+  my $max_lang;
+  my $max_count = 0;
+  for my $lang (keys %count) {
+    if ($count{$lang} > $max_count) {
+      $max_count = $count{$lang};
+      $max_lang  = $lang;
+    }
   }
-  elsif ($txt =~ /\p{Script=Hangul}/) {
-    return KOREAN;
-  }
-  elsif ($txt =~ /\p{Script=Cyrillic}/) {
-    return RUSSIAN;
-  }
-  else {
-    return ENGLISH;
-  }
+
+  return $max_lang || ENGLISH;
 }
 
 1;
