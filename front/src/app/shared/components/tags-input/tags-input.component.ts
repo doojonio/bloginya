fd4031ac1647881d9d73e108e77f10bfa81210eb
Anton Fedotov <tosha.fedotov.2000@gmail.com>
2025-09-27 16:28:32 +0500

    tags

---
+++ front/src/app/shared/components/tags-input/tags-input.component.ts
@@ -0,0 +1,73 @@
+import { COMMA, ENTER, SPACE } from '@angular/cdk/keycodes';
+import { Component, input, model } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatChipInputEvent, MatChipsModule } from '@angular/material/chips';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-tags-input',
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatChipsModule,
+    MatIconModule,
+  ],
+  template: `
+    <mat-form-field class="tags-field">
+      <mat-chip-grid #chipGrid [formControl]="control()">
+        @for (tag of tags(); track tag) {
+        <mat-chip-row (removed)="removeTag(tag)">
+          {{ tag }}
+          <button matChipRemove>
+            <mat-icon>cancel</mat-icon>
+          </button>
+        </mat-chip-row>
+        }
+        <input
+          placeholder="New tag..."
+          [matChipInputFor]="chipGrid"
+          [matChipInputSeparatorKeyCodes]="separatorKeysCodes"
+          (matChipInputTokenEnd)="addTag($event)"
+        />
+      </mat-chip-grid>
+    </mat-form-field>
+  `,
+  styles: `
+    :host {
+      display: block;
+    }
+  `,
+})
+export class TagsInputComponent {
+  tags = model.required<string[]>();
+  control = input.required<FormControl>();
+  separatorKeysCodes = [ENTER, COMMA, SPACE] as const;
+
+  removeTag(tag: string) {
+    this.tags.update((tags) => {
+      const idx = tags.indexOf(tag);
+      if (idx < 0) {
+        return tags;
+      }
+
+      tags.splice(idx, 1);
+      return [...tags];
+    });
+  }
+
+  addTag(event: MatChipInputEvent) {
+    const tag = (event.value || '').trim();
+    if (tag.length < 3) {
+      return;
+    }
+
+    this.tags.update((tags) => {
+      if (tags.indexOf(tag) >= 0) {
+        return tags;
+      }
+      return [...tags, tag];
+    });
+    event.chipInput.clear();
+  }
+}
